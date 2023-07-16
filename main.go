3fd589f484f027f98d8a55c218bfeb468c5e770b
D-Zane <theangryzane@gmail.com>
2023-07-17 00:49:34 +0300

    Добавление 2-ух лейблов

--- main.go
+++ main.go
@@ -1,9 +1,22 @@
 package main
 
-import "fyne.io/fyne/v2/app"
+import (
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
+)
 
 func main() {
 	a := app.New()
 	w := a.NewWindow("MyApp")
+
+	label := widget.NewLabel("Test1")
+	label2 := widget.NewLabel("Test2")
+
+	w.SetContent(container.NewVBox(
+		label,
+		label2,
+	))
+
 	w.ShowAndRun()
 }
